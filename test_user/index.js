888aadd43aeb3765a1e5f88d08cc1ede9397adfd
jiangxue93 <bupt_jx@qq.com>
2021-07-05 16:27:07 +0800

    新增组件test user

---
+++ test_user/index.js
@@ -0,0 +1,13 @@
+import react, { useEffect } from 'react'
+
+const Test_User = () => {
+  useEffect(() => {
+    console.log(123)
+  }, [])
+
+  return (
+    <>123123</>
+  )
+}
+
+export default Test_User
